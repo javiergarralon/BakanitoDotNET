6a1d962b862c23a248f3fed6ac88b68d1e75824a
javiergarralon <garralonmourin@gmail.com>
2024-09-17 10:03:49 +0200

    Button for Create Category & ActionController

--- CategoryCRUD/Controllers/CategoryController.cs
+++ CategoryCRUD/Controllers/CategoryController.cs
@@ -7,14 +7,20 @@ namespace CategoryCRUD.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) {
-            _db = db;   
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList(); 
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
