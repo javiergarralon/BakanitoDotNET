80e9b31e22600a3fe0c695d072b0ca8716b6c58a
javiergarralon <garralonmourin@gmail.com>
2024-09-18 11:58:37 +0200

    [Temp] Razor Project - Create Razor Page & Bug Fixes BakanitoWeb Index and Create

---
+++ BakanitoWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using BakanitoWebRazor_Temp.Data;
+using BakanitoWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakanitoWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);  
+            _db.SaveChanges();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
