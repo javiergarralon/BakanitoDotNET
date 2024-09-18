b5902220032604716abf32e8a1cf430ff3fe48f6
javiergarralon <garralonmourin@gmail.com>
2024-09-18 12:54:04 +0200

    [Temp] Razor Project - Delete Categories & BakanitoWeb Delete Bug Fixes

---
+++ BakanitoWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using BakanitoWebRazor_Temp.Data;
+using BakanitoWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakanitoWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? categorySelected = _db.Categories.Find(Category.Id);
+
+            if (categorySelected == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categorySelected);
+            _db.SaveChanges();
+            //TempData["success"] = "Category deleted successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
+
