d0de1d48c5426af35bc501a75e84a97b5a7de9f1
javiergarralon <garralonmourin@gmail.com>
2024-09-18 11:44:44 +0200

    [Temp] Razor Project - Category Index Page

---
+++ BakanitoWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using BakanitoWebRazor_Temp.Data;
+using BakanitoWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakanitoWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
