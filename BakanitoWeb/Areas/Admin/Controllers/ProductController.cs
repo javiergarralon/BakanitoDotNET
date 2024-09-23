b00b03b4f651a6cd20871d7cb36954c3bfc650b9
javiergarralon <garralonmourin@gmail.com>
2024-09-23 18:49:36 +0200

    Load File & Save in wwwroot path

--- BakanitoWeb/Areas/Admin/Controllers/ProductController.cs
+++ BakanitoWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,12 @@ namespace BakanitoWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -54,6 +57,19 @@ namespace BakanitoWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file!= null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productViewModel.Product.ImageUrl = @"\images\product" + fileName;
+                }
+
                 _unitOfWork.ProductRepository.Add(productViewModel.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
