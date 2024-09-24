9a38d6262e4c18f524e2d737dbae9861e931c66f
javiergarralon <garralonmourin@gmail.com>
2024-09-24 16:56:50 +0200

    Add Delete Confirmation with SweetAlert2

--- BakanitoWeb/wwwroot/js/product.js
+++ BakanitoWeb/wwwroot/js/product.js
@@ -9,10 +9,10 @@ function loadDataTable() {
         "ajax": { url: '/admin/product/getall' },
         "columns": [
             { data: 'title', "width": "25%" },
-            { data: 'isbn', "width": "20%" },
-            { data: 'listPrice', "width": "5%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
@@ -21,9 +21,31 @@ function loadDataTable() {
                      <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
-                "width": "15%"
+                "width": "25%"
             }
         ]
     });
 }
 
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}
