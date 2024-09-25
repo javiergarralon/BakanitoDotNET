ae34973f534a826893bb227585c23e5a164652e4
javiergarralon <garralonmourin@gmail.com>
2024-09-25 14:56:45 +0200

    Fix ApplicationDbContext

--- Bakanito.DataAccess/Data/ApplicationDbContext.cs
+++ Bakanito.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Bakanito.Models.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bakanito.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
