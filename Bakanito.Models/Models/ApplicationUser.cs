7bfe9ed14c033c29f6cfbede9cbcf4bef073d995
javiergarralon <garralonmourin@gmail.com>
2024-09-25 17:41:59 +0200

    ApplicationUser extends IdentityUser

--- Bakanito.Models/Models/ApplicationUser.cs
+++ Bakanito.Models/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,11 @@ namespace Bakanito.Models.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public int Name { get; set; }  
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string?  PostalCode { get; set; }
     }
 }
